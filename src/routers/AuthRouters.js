628800590dd02d86dbeb6616bba3a3f97eeb0704
AnonComrade993 <anoncomrade993@gmail.com>
2024-12-06 13:01:15 +0100

    update

--- src/routers/AuthRouters.js
+++ src/routers/AuthRouters.js
@@ -20,7 +20,7 @@ const {
 	cancelDeleteUser
 } = require('../controllers/AuthControllers.js')
 
-const { tokenRequestLimiter } = require('../middlewares/security.js')
+//const { tokenRequestLimiter } = require('../middlewares/security.js')
 
 
 authRouter.post('/register', registerUser);
@@ -31,11 +31,11 @@ authRouter.post('/change-email', requireAuth, changeEmail)
 authRouter.post('/change-password', requireAuth, changePassword)
 authRouter.post('/request-user-delete', requireAuth, deleteUser)
 authRouter.post('/cancel-user-delete', cancelDeleteUser)
-authRouter.post('/reset-password',forgotPassword)
-authRouter.post('/request-fp-token', tokenRequestLimiter, request_fp_OTP)
-authRouter.post('/request-ev-token', tokenRequestLimiter, request_ev_OTP)
-authRouter.post('/request-ce-token', tokenRequestLimiter, requireAuth, request_ce_OTP)
-authRouter.post('/request-cp-token', tokenRequestLimiter, requireAuth, request_cp_OTP)
+authRouter.post('/reset-password', forgotPassword)
+authRouter.post('/request-fp-token', /*tokenRequestLimiter , */ request_fp_OTP)
+authRouter.post('/request-ev-token', /*tokenRequestLimiter ,*/ request_ev_OTP)
+authRouter.post('/request-ce-token', /*tokenRequestLimiter,*/ requireAuth, request_ce_OTP)
+authRouter.post('/request-cp-token', /*tokenRequestLimiter, */ requireAuth, request_cp_OTP)
 
 authRouter.get('/profile', requireAuth, getUserProfile)
 
