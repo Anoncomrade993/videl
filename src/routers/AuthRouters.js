f6c596497856c362a18e5085eb79e49f2dbe922a
AnonComrade993 <anoncomrade993@gmail.com>
2024-12-21 20:36:03 +0100

    update AuthRouters.js

--- src/routers/AuthRouters.js
+++ src/routers/AuthRouters.js
@@ -37,7 +37,7 @@ const {
 
 authRouter.post('/register', registerUser);
 authRouter.post('/signin', loginUser)
-authRouter.post('/signout', requireAuth, logoutUser)
+authRouter.get('/signout', requireAuth, logoutUser)
 
 
 
